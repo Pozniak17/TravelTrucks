aa30a568cbb1541340c76d477ffc676fda1c4a82
Yevhenii Pozniak <jekamanu@gmail.com>
2025-06-12 13:37:32 +0300

    update

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -9,23 +9,13 @@ export default function Catalog() {
   const dispatch = useDispatch();
   const campers = useSelector((state) => state.campers.items);
   const { isLoading, error } = useSelector((state) => state.campers);
-  // const location = useSelector((state) => state.filters.location);
   const filters = useSelector((state) => state.filters);
-  console.log(filters);
 
   const [limit, setLimit] = useState(4);
 
   useEffect(() => {
     dispatch(fetchCampers({ limit, filters }));
   }, [dispatch, limit, filters]);
-
-  // useEffect(() => {
-  //   if (filters) {
-  //     dispatch(fetchCampersByFilters({ limit, filters }));
-  //   } else {
-  //     dispatch(fetchCampers({ limit }));
-  //   }
-  // }, [dispatch, limit, filters]);
 
   const loadMore = () => {
     setLimit((limit) => limit + 4);
