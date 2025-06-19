f121fbf515fa31b4e137eb8c322272c68dcc7217
Yevhenii Pozniak <jekamanu@gmail.com>
2025-06-19 11:36:44 +0300

    update

--- src/pages/Favorites/Favorites.styled.js
+++ src/pages/Favorites/Favorites.styled.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  background-color: thistle;
+  background: linear-gradient(to bottom, thistle, plum, darkslateblue);
+
   height: 100%;
 
   padding: 48px 64px;
